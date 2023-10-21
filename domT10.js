f8cf5be13ed23e45d1d384c8fcbf5c15c6b14228
ShatakshiN <shatakshinimare27@gmail.com>
2023-10-21 21:13:17 +0530

    by mistake did change

--- domT10.js
+++ domT10.js
@@ -6,6 +6,7 @@ function storage(e){
     e.preventDefault();
     var namee = document.getElementById('name').value;
     var mail = document.getElementById('email').value;
+    
 
     localStorage.setItem('name',namee);
     localStorage.setItem('email',mail);
